655c892f1593a049e9b34616896be623e5ae4fee
Gabriel Vinicius Strapasson <gabri.vstrapasson@gmail.com>
2025-07-03 17:19:42 -0300

    Ajustes Rota API

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,57 +2,61 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const cityName = document.querySelector('#city_name').value;
+    const country = document.querySelector('#state_code').value;
 
     if (!cityName) {
         document.querySelector('#weather').classList.remove('show');
-        showAlert('Por favor, insira o nome de uma cidade.');
+        showAlert(`Por favor, insira o nome de uma cidade.            
+            <img src="src/images/complete-form.svg"/> 
+            `);
         return;
     }
     
     const apiKey = 'f5b6a74dac7a33dbf132ca8ce8693fcb';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)},${encodeURI(country)}&appid=${apiKey}&units=metric&lang=pt_br`;
 
     const results = await fetch(apiUrl);
     const json = await results.json();
 
-    if (json.cod !== 200) {
-        document.querySelector('#weather').classList.remove('show');
-        showAlert(`Cidade não encontrada...
-            <img src="src/images/404.svg"/> 
-            `);
-
-    } else {
+    if (json.cod === 200) {
         showInfo({
             city: json.name,
             country: json.sys.country,
             temp: json.main.temp,
-            temp_min: json.main.temp_min,
-            temp_max: json.main.temp_max,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
             description: json.weather[0].description,
             tempIcon: json.weather[0].icon,
-            wind_speed: json.wind.speed,
+            windSpeed: json.wind.speed,
             humidity: json.main.humidity,
         });
-    }
+    } else {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert(`
+            Não foi possível localizar...
 
+            <img src="src/images/404.svg"/>
+        `)
+    }
     
 });
 
 function showInfo(json){
     showAlert('');
 
-    document.querySelector('#weather').classList.add('show');
+    document.querySelector("#weather").classList.add('show');
+
     document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
-    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')}<sup>°</sup>C`;
-    document.querySelector('#temp_description').innerHTML = json.description.charAt(0).toUpperCase() + json.description.slice(1);
-    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
-    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')}<sup>°</sup>C`;
-    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')}<sup>°</sup>C`;
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_description').innerHTML = `${json.description}`;
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
     document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
-    document.querySelector('#wind_speed').innerHTML = `${json.wind_speed.toFixed(1)} km/h`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
 }
-
 function showAlert(message) {
     document.querySelector('#alert').innerHTML = message;
 }
